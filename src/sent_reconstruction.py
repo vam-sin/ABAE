fbf98c047d564d86f7dd49d658133d0b91612dfe
vam-sin <vamsinallapareddy@gmail.com>
2019-11-19 11:42:02 +0530

    doc: Renames sent_reconst to calculate_rs in sent_reconstruction.py file

--- src/sent_reconstruction.py
+++ src/sent_reconstruction.py
@@ -5,8 +5,9 @@ import numpy as np
 def calculate_pt(W, zs, b):
     return softmax(np.matmul(W, zs) + b)
 
-def sent_reconst(T, W, zs, b):
+def calculate_rs(T, W, zs, b):
     T = T.T
     pt = calculate_pt(W, zs, b)
+    rs = np.matmul(T, pt)
 
-    return np.matmul(T, pt)
+    return rs
--- train.py
+++ train.py
@@ -3,6 +3,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 import numpy as np
+import pickle
 
 # Tasks
 # 1. Use the functions to get the aspects.
@@ -11,7 +12,42 @@ import numpy as np
 
 # Hyperparameters
 
-# Train
+# Model
+class Model(nn.Module):
+
+    def __init__(self, M, E, unique_words, T, W, b):
+        self.M = M
+        self.E = E
+        self.unique_words = unique_words
+        self.T = T
+        self.W = W
+        self.b = b
+
+    def forward(self, input):
+        # Forward pass of the review given
+        ys = calculate_ys(input, self.E, self.unique_words)
+        zs = calculate_zs(input, self.M, ys, self.E, self.unique_words)
+        rs = calculate_rs()
+
+
+
+# Training of the Model
+M = np.random.randn(200, 200)
+# W declaration
+# b declaration
+
+infile = open('src/E.pickle','rb')
+E = pickle.load(infile)
+infile.close()
+
 infile = open('src/reviews.pickle','rb')
 reviews = pickle.load(infile)
+infile.close()
+
+infile = open('src/unique_words.pickle','rb')
+unique_words = pickle.load(infile)
+infile.close()
+
+infile = open('src/T.pickle','rb')
+T = pickle.load(infile)
 infile.close()
